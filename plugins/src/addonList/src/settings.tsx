79287cdfac4dea71b23dd8c08b6581ecf99c14ff
NightNutSky <snackyt1997@gmail.com>
2023-12-30 23:57:59 +0300

    30.12.2023
    - Fix Dark Vendetta theme (not finished yet)
    - Update Addon List plugin's settings section

--- plugins/src/addonList/src/settings.tsx
+++ plugins/src/addonList/src/settings.tsx
@@ -1,24 +1,41 @@
 import { ReactNative } from "@vendetta/metro/common";
 import { storage } from "@vendetta/plugin";
 import { useProxy } from "@vendetta/storage";
+import { getAssetIDByName } from "@vendetta/ui/assets";
 import { ErrorBoundary, Forms } from "@vendetta/ui/components";
+
+const { FormSwitchRow, FormIcon, FormSection } = Forms;
+const Icons = {
+    List: getAssetIDByName('ic_list')
+};
 
 export default () => {
     useProxy(storage);
 
     return (
         <ErrorBoundary>
-            <ReactNative.ScrollView>
-                <Forms.FormSwitchRow
-                    label="Plugin List: Always send detailed list"
-                    onValueChange={(value: boolean) => storage.pluginListAlwaysDetailed = value}
-                    value={storage.pluginListAlwaysDetailed}
-                />
-                <Forms.FormSwitchRow
-                    label="Theme List: Always send detailed list"
-                    onValueChange={(value: boolean) => storage.themeListAlwaysDetailed = value}
-                    value={storage.themeListAlwaysDetailed}
-                />
+            <ReactNative.ScrollView style={{ flex: 1 }}>
+
+                <FormSection title="Plugin List" titleStyleType="no_border" >
+                    <FormSwitchRow
+                        label="Always send detailed list"
+                        subLabel='This will keep the "detailed" option always set to "True"'
+                        leading={<FormIcon source={Icons.List} />}
+                        onValueChange={(value: boolean) => storage.pluginListAlwaysDetailed = value}
+                        value={storage.pluginListAlwaysDetailed}
+                    />
+                </FormSection>
+
+                <FormSection title="Theme List" titleStyleType="no_border" >
+                    <FormSwitchRow
+                        label="Always send detailed list"
+                        subLabel='This will keep the "detailed" option always set to "True"'
+                        leading={<FormIcon source={Icons.List} />}
+                        onValueChange={(value: boolean) => storage.themeListAlwaysDetailed = value}
+                        value={storage.themeListAlwaysDetailed}
+                    />
+                </FormSection>
+
             </ReactNative.ScrollView>
         </ErrorBoundary>
     );
