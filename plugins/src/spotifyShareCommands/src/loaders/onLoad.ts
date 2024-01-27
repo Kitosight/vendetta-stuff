874ee4f9b44d3cb95fbf4e21787861dd5cdd4ebb
NightNutSky <snackyt1997@gmail.com>
2024-01-27 22:29:21 +0300

    chore(plugins): Spotify Share Commands

--- plugins/src/spotifyShareCommands/src/loaders/onLoad.ts
+++ plugins/src/spotifyShareCommands/src/loaders/onLoad.ts
@@ -7,41 +7,26 @@ import { type CommandName, execute } from "../commands";
 export function load(): RegisteredCommand[] {
     const registeredCommands: RegisteredCommand[] = [];
 
-    const commonProperties = <ApplicationCommand> {
-        inputType: 1,
-        type: 1,
-        applicationId: '-1'
-    };
-
-    function shortCommandData(name: CommandName, description: string) {
+    function formCommandData(name: CommandName, description: string) {
         return <ApplicationCommand> {
             name,
             displayName: name,
             description,
             displayDescription: description,
-            execute: async (_args, ctx) => execute(name, ctx) 
+            inputType: 1,
+            type: 1,
+            applicationId: '-1',
+            execute: async (_args, ctx) => execute(name, ctx)
         };
     }
 
     const { SpotifyArtist, SpotifyAlbum, SpotifyTrack, SpotifyCover } = Command;
 
     registeredCommands.push(
-        registerCommand({
-            ...shortCommandData(SpotifyArtist.name, SpotifyArtist.description),
-            ...commonProperties
-        }),
-        registerCommand({
-            ...shortCommandData(SpotifyAlbum.name, SpotifyAlbum.description),
-            ...commonProperties
-        }),
-        registerCommand({
-            ...shortCommandData(SpotifyTrack.name, SpotifyTrack.description),
-            ...commonProperties
-        }),
-        registerCommand({
-            ...shortCommandData(SpotifyCover.name, SpotifyCover.description),
-            ...commonProperties
-        })
+        registerCommand({...formCommandData(SpotifyArtist.name, SpotifyArtist.description)}),
+        registerCommand({...formCommandData(SpotifyAlbum.name, SpotifyAlbum.description)}),
+        registerCommand({...formCommandData(SpotifyTrack.name, SpotifyTrack.description)}),
+        registerCommand({...formCommandData(SpotifyCover.name, SpotifyCover.description)})
     );
 
     return registeredCommands;
